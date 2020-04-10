0c25d8e7db6082334cf7312b6d4c22b162473d78
Shukun_Zhang <34422187+ShukunZhang@users.noreply.github.com>
2020-04-11 02:25:02 +0800

    Add descriptions in Doxygen Format

--- src/AnomalyDetectionSystem.h
+++ src/AnomalyDetectionSystem.h
@@ -10,44 +10,49 @@ class AnomalyDetectionSystem
 {
 public:
 
-    /* 
-     * The main reporting mechanism in the library - when an anomaly is detected per process_input,
+    /*！ 
+     * \brief The main reporting mechanism in the library - when an anomaly is detected per process_input,
      * these type of functions are called by ADS where model_results are internal underlying model
      * values (neeeded for process_feedback) and data is a feature-expanded representation of the
      * corresponding data.
      */
 	typedef void (*anomaly_detected_call_func)(vector<double> model_results, vector<double> data);
 
-    /*
-     * Constructs an anomaly detection system using the settings provided by the file named by
+    /*!
+     * \brief Constructs an anomaly detection system using the settings provided by the file named by
      * file_name. Note that file_name is optional and that default settings will be used if not
      * provided.
      */
 	AnomalyDetectionSystem(string file_name = "");
 
 
-    /*
-     * Note that AnomalyDetectionSystem does allocate memory, so the destructor is non-empty.
+    /*!
+     * \brief Note that AnomalyDetectionSystem does allocate memory, so the destructor is non-empty.
      */
 	~AnomalyDetectionSystem();
 
     // recover the Anomaly Detection System from a prviously saved state
 	void initalize_from_saved_state(string saved_state);
 
-    /*
-     * The anomaly detection system processes the next provided data vector. Note that per temporal
+    /*!
+     * \brief The anomaly detection system processes the next provided data vector. Note that per temporal
      * information, only one sample can be processed at a time. In lieu of returning a result,
      * the provided anomaly_detected_call_func func will be called with the model information
      * and the processed data vector if and only if an anomaly is detected. 
+     * \param data a vector of doubles, the input data
+     * \param func the function called when anomaly detected
      */
 	void process_input(vector<double> data, anomaly_detected_call_func func);
 
 
-    /*
-     * After process_input has used its provided anomaly_detected_call_func to indicate an anamoly,
+    /*!
+     * \brief After process_input has used its provided anomaly_detected_call_func to indicate an anamoly,
      * process_feedback can be called with the provided model_results and data to the 
      * anomaly_detected_call_func once the user has indicated that the underlying data either is
      * or is not anamalous. In cases where model_results are null, one cannot call process_feedback.
+     * \param model_results a vector of doubles constains the results from each model
+     * \param data, a vector of doubles, the input data
+     * \param isAnomaly, true if it is anomaly
      */
 	void process_feedback(vector<double> model_results, vector<double> data, bool isAnomaly);
 
